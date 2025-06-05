55ce5a373cea3b9b079b6c3d4742fdabad1b9688
Setho <retsudog@gmail.com>
2025-06-05 12:50:27 +1000

    Adding circle, player classes. Draw player

--- main.py
+++ main.py
@@ -3,21 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
 
 
 if __name__ == "__main__":
